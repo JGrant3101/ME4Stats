de38e6942fead15fbce0207e5d828a093b7f15ff
JGrant3101 <joegrant01423@gmail.com>
2023-11-28 22:46:43 +0000

    Finishing computing tutorial 2

--- ComputingTutorial2/ComputingTutorial2.m
+++ ComputingTutorial2/ComputingTutorial2.m
@@ -34,9 +34,14 @@ for i = 1:1000
 end
 Onetestsimresults = Onetestsimresults';
 OnetestFractions = OnetestFractions';
-figure 
+figure
+tiledlayout(1, 2)
+nexttile
 % Plotting a histogram
 histogram(Onetestsimresults)
+xlabel('Athletes testing positive')
+ylabel('Frequency')
+title('Histogram of 1 test results')
 
 % Now want to simulate a situation where you have to fail two tests in
 % order to be classified as having taken drugs
@@ -58,9 +63,12 @@ for i = 1:1000
 end
 Twotestsimresults = Twotestsimresults';
 TwotestFractions = TwotestFractions';
-figure
+nexttile
 % Plotting a histogram
 histogram(Twotestsimresults)
+xlabel('Athletes testing positive')
+ylabel('Frequency')
+title('Histogram of 2 test results')
 
 % Bayes theorem to work out the conditional probability of an athlete
 % having done drugs given they did one test and failed it
@@ -69,11 +77,86 @@ disp(BayesOnetest)
 disp(mean(OnetestFractions)) 
 
 % Bayes theorem to work out the conditional probability of an athlete
-% having done drugs given they did one test and failed it
+% having done drugs given they did two test and failed it
 BayesTwotest = (0.64*0.1)/0.0676;
 disp(BayesTwotest)
 disp(mean(TwotestFractions))
 
 %% Question 2 - Probability distributions
+% Setting up an input x vector
+x1 = linspace(0, 12, 13)';
+x2 = linspace(-10, 10, 100)';
 
+% Getting geometric pdf values
+geopdfresults = geopdf(x1, 0.3);
+% Getting geometric cdf values
+geocdfresults = geocdf(x1, 0.3);
 
+% Getting normal pdf values
+normpdfresults = normpdf(x2, 0, 2);
+% Getting normal cdf values
+normcdfresults = normcdf(x2, 0, 2);
+
+% Plotting
+figure
+tiledlayout(2, 2)
+% Geometric pdf
+nexttile
+plot(x1, geopdfresults)
+title('Geometric distribution pdf')
+xlabel('x value')
+ylabel('probability')
+% Geometric cdf
+nexttile
+plot(x1, geocdfresults)
+title('Geometric distribution cdf')
+xlabel('x value')
+ylabel('probability')
+% Normal pdf
+nexttile
+plot(x2, normpdfresults)
+title('Normal distribution pdf')
+xlabel('x value')
+ylabel('probability')
+% Normal cdf
+nexttile
+plot(x2, normcdfresults)
+title('Normal distribution cdf')
+xlabel('x value')
+ylabel('probability')
+
+% Generating random numbers from each distribution
+samples = 1000;
+georndresults = geornd(0.3, [samples, 1]);
+normrndresults = normrnd(0, 2, [samples, 1]);
+
+% Plotting 
+figure
+tiledlayout(1, 2)
+nexttile
+histogram(georndresults, 'Normalization', 'pdf')
+hold on
+plot(x1, geopdfresults)
+title('Histogram of random values from geometric distribution')
+xlabel('x values')
+ylabel('relative frequency')
+
+nexttile
+histogram(normrndresults, 'Normalization', 'pdf')
+hold on
+plot(x2, normpdfresults)
+title('Histogram of random values from normal distribution')
+xlabel('x values')
+ylabel('relative frequency')
+
+% Calculating means and standard deviations for the randomly generated
+% values
+georndmean = sum(georndresults)/samples;
+disp(georndmean)
+georndstd = sqrt(sum((georndresults-georndmean).^2)/(samples-1));
+disp(georndstd)
+
+normrndmean = sum(normrndresults)/samples;
+disp(normrndmean)
+normrndstd = sqrt(sum((normrndresults-normrndmean).^2)/(samples-1));
+disp(normrndstd)
